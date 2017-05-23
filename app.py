1c872f8088ff6f9dba6ad20dada3816259e404e4
PriyankaM3 <priya.mgdm@gmail.com>
2017-05-23 15:22:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,14 +60,7 @@ def makeYqlQuery(req):
 
 
 def makeWebhookResult(data):
-    income = 1800
-    expense = 600
-    age = 30
-    retirementAge = 65
-    
-    speech = "As you still have " + retirementAge-age + "years for your retirement, you can save " + (income-expense)*0.10 + "$ every month." 
-    
-   # speech = "You need to save 1000$ every month towars your retirement."
+    speech = "You need to save 1000$ every month towards your retirement."
 
     return {
         "speech": speech,
